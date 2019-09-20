d28df17a51532cd5576b853603606d1d831b7dd5
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-20 07:41:19 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,7 @@ class TicTacToe
   end
 
   def turn_count
-    @board.count {|token| token == "X"|| token == "O"}
+    @board.count { |token| token == "X" || token == "O" }
   end
 
   def current_player
