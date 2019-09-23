836e6310bef00c26dccad03f006b5fb0aa00bcd4
Craig Christopher Messi <craig.messi@gmail.com>
2019-09-23 14:52:45 +0000

    Done.

--- spec/01_tic_tac_toe_spec.rb
+++ spec/01_tic_tac_toe_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/tic_tac_toe.rb'
 
-describe './lib/tic_tac_toe.rb' do
+escribe './lib/tic_tac_toe.rb' do
   describe TicTacToe do
     describe '#initialize' do
       it 'assigns an instance variable @board to an array with 9 blank spaces " "' do
